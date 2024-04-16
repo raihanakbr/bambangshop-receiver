385218d40ce2c32ff28cc585cdc4a67de50aa6f3
raihanakbr <muhammad.raihan22@ui.ac.id>
2024-04-16 20:43:09 +0700

    Implement receive_notification function in Notification service.

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -92,4 +92,9 @@ impl NotificationService {
             ))  
         }
     }
+
+    pub fn receive_notification(payload: Notification) -> Result<Notification> {
+        let subscriber_result: Notification = NotificationRepository::add(payload);
+        return Ok(subscriber_result);
+    }
 }
